9aa18b6dece0cddd890131f0a263614905bf80c9
zhangxiaohui <809887981@qq.com>
2021-01-22 21:24:31 +0800

    修复问题

---
+++ app/src/main/java/com/xh/android/MiBaseUrlProvider.kt
@@ -0,0 +1,11 @@
+package com.xh.android
+
+import com.h.android.http.UrlProvider
+
+/**
+ *2021/1/22
+ *@author zhangxiaohui
+ *@describe
+ */
+interface MiBaseUrlProvider : UrlProvider {
+}
---
+++ app/src/main/java/com/xh/android/TestApiService.kt
@@ -0,0 +1,12 @@
+package com.xh.android
+
+import com.h.android.http.interceptor.BaseUrlProvider
+
+/**
+ *2021/1/22
+ *@author zhangxiaohui
+ *@describe
+ */
+@BaseUrlProvider(MiBaseUrlProvider::class)
+interface TestApiService {
+}
